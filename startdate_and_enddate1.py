ed3da430f0d2e9c4a1fef5884eb01550aa4303e1
og80218 <65637451+og80218@users.noreply.github.com>
2020-05-22 19:07:39 +0800

    Update startdate_and_enddate1.py
    #日期設當天

--- startdate_and_enddate1.py
+++ startdate_and_enddate1.py
@@ -1,27 +1,27 @@
-#給予起始時間及結束時間
+#日期設當天
 
 import datetime
 
 #西元改民國
 def turnyear(A):
-    y = str( int( A.split( '-' )[0] ) - 1911 )
-    m = A.split( '-' )[1]
-    d = A.split( '-' )[2]
+    y = str( int( A[0:4] ) - 1911 )
+    m = A[4:6]
+    d = A[6:8]
     B = y + '/' + m + '/' + d
     return B
 
-
 def create_assist_date(datestart=None, dateend=None):
     if datestart is None:
-        datestart = '2016-10-03'          #輸入起始西元年月份
-    if dateend is None:
-        dateend = datetime.datetime.now().strftime( '2016-10-04' )          #輸入結束西元年月份
+        datestart = str(datetime.datetime.now()).split(' ')[0].replace('-','')
 
-    datestart = datetime.datetime.strptime( datestart, '%Y-%m-%d' )
-    dateend = datetime.datetime.strptime( dateend, '%Y-%m-%d' )
+    if dateend is None:
+        dateend = datetime.datetime.now().strftime( '%Y%m%d' )
+
+    datestart = datetime.datetime.strptime( datestart, '%Y%m%d' )
+    dateend = datetime.datetime.strptime( dateend, '%Y%m%d' )
     date_list = []
-    date_list.append( datestart.strftime( '%Y-%m-%d' ) )
+    date_list.append( datestart.strftime( '%Y%m%d' ) )
     while datestart < dateend:
         datestart += datetime.timedelta( days=+1 )
-        date_list.append( datestart.strftime( '%Y-%m-%d' ) )
+        date_list.append( datestart.strftime( '%Y%m%d' ) )
     return date_list
